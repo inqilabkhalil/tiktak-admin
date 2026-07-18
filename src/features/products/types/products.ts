60a3d9cc01de78d0cd3a5ff31dc4f1718e891a00
kenul09 <konull0909@gmail.com>
2026-07-19 00:04:01 +0400

    feat: add products page with mock service and types

---
+++ src/features/products/types/products.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  price: string;
+  category: string;
+  type: string;
+  createdAt: string;
+}
