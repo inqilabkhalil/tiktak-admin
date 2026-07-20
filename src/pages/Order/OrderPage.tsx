3bb3e9ad6ba13828b1a7ebc96cd77c69579dd4ab
inqilabkhalil <inqlab.xelilov91@gmail.com>
2026-07-21 02:39:54 +0400

    fix

--- src/pages/Order/OrderPage.tsx
+++ src/pages/Order/OrderPage.tsx
@@ -1,20 +1,5 @@
 import React from 'react';
-import styles from './Order.module.css';
-import { OrderStats } from '@/features/orders/components/OrderStats/OrderStats';
-import { OrderFilters } from '@/features/orders/components/OrderFilters/OrderFilters';
-import { OrderTable } from '@/features/orders/components/OrderTable/OrderTable';
-import { OrderPagination } from '@/features/orders/components/OrderPagination/OrderPagination';
 
 export const OrdersPage: React.FC = () => {
-  return (
-    <div className={styles.pageContainer}>
-      <h1 className={styles.pageTitle}>Sifarişlər</h1>
-      <OrderStats />
-      <div className={styles.tableCardWrapper}>
-        <OrderFilters />
-        <OrderTable />
-        <OrderPagination />
-      </div>
-    </div>
-  );
+  return <div>Orders - yenidən yazılacaq</div>;
 };
