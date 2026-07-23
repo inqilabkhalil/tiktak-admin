6443ed979c40cbe8bb582d121260d50c0c42d995
vaqif mohbaliyev <vagifmohbaliyev45@gmail.com>
2026-07-23 22:37:49 +0400

    add protected

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,26 +1,34 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
 
 import { CampaignsPage } from '@/pages/campaigns/campaign';
 import CategoriesPage from '@/pages/categories/categories';
 import ProductsPage from '@/pages/products/products';
-
-import UsersPage from '@/pages/users';
-import { Layout } from '@/shared/components/Layout';
-import Login from '@/pages/auth';
 import { OrdersPage } from '@/pages/orders/orders';
+import UsersPage from '@/pages/users';
+import Login from '@/pages/auth';
 
+import { Layout } from '@/shared/components/Layout';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/auth" replace />} />
-      <Route element={<Layout />}>
+
+      <Route
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
         <Route path="/campaigns" element={<CampaignsPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/users" element={<UsersPage />} />
       </Route>
+
       <Route path="/auth" element={<Login />} />
     </Routes>
   );
