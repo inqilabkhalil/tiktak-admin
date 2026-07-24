ec2541ed732ae1f3a52db2507f9763818df1ae3c
inqilabkhalil <inqlab.xelilov91@gmail.com>
2026-07-24 13:01:03 +0400

    your message

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -6,10 +6,9 @@ const api = axios.create({
 
 // Müvəqqəti token — login/localStorage axını hazır olana qədər
             
-const TEMP_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6Iis5OTQxMDU1NTQ0MjIiLCJzdWIiOjEsImlhdCI6MTc4NDgwNjQ1OCwiZXhwIjoxNzg0ODQ5NjU4fQ.Wy6HGgq9PRZw7RlRCdHvhXFl4yMLWs4xonH83PVhKMI"
 // Hər sorğuda avtomatik token əlavə edir
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token') ?? TEMP_ACCESS_TOKEN;
+  const token = localStorage.getItem('access_token');
 
   console.log('TOKEN:', token);
   if (token) {
